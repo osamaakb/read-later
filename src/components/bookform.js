44d64bce9880f769fd60975d1871e98b5c167361
osamaakb <osama98akb@gmail.com>
2020-12-17 12:42:42 +0300

    new commit from codesandbox

--- src/components/bookform.js
+++ src/components/bookform.js
@@ -1,6 +1,6 @@
-import React, { useState, useContext } from 'react';
-import { Form, Button, Col, Row } from 'react-bootstrap'
-import API from '../API';
+import React, { useState, useContext } from "react";
+import { Form, Button, Col, Row } from "react-bootstrap";
+import API from "../API";
 import Context from "../stateProvider";
 
 const BookForm = () => {
@@ -40,30 +40,39 @@ const BookForm = () => {
         });
     }
 
-    return (
-        <div className="m-3">
-            <Form onSubmit={pushBook}>
-                <Row>
-                    <Col>
-                        <Form.Group controlId="formBasicEmail">
-                            <Form.Label>Book Name</Form.Label>
-                            <Form.Control value={book.title} onChange={addBook} id="title" type="text" placeholder="Book Name" />
-                        </Form.Group>
-                    </Col>
-                    <Col>
-                        <Form.Group controlId="formBasicPassword">
-                            <Form.Label>Author</Form.Label>
-                            <Form.Control value={book.author} onChange={addBook} id="author" type="text" placeholder="Auth" />
-                        </Form.Group>
-                    </Col>
-                </Row>
-                <Button variant="dark" type="submit">
-                    Add book
-  </Button>
-            </Form>
-        </div>
-    );
-}
-
-
-export default BookForm;
+  return (
+    <div className="m-3">
+      <Form onSubmit={pushBook}>
+        <Row>
+          <Col>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Book Name</Form.Label>
+              <Form.Control
+                onChange={addBook}
+                value={book.title}
+                id="title"
+                type="text"
+                placeholder="Book Name"
+              />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Author</Form.Label>
+              <Form.Control
+                onChange={addBook}
+                value={book.author}
+                id="author"
+                type="text"
+                placeholder="Author"
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Button variant="dark" type="submit">
+          Add book
+        </Button>
+      </Form>
+    </div>
+  );
+};
--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,23 @@
-import * as firebase from 'firebase/app';
-import 'firebase/firestore';
+import * as firebase from "firebase/app";
+import "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBNGYwp9XI5C2jbsYZjOJc3ydVN-2g3vjI",
-    authDomain: "readlater-708be.firebaseapp.com",
-    databaseURL: "https://readlater-708be.firebaseio.com",
-    projectId: "readlater-708be",
-    storageBucket: "readlater-708be.appspot.com",
-    messagingSenderId: "264923569873",
-    appId: "1:264923569873:web:aa099d638c43334ad52ff0",
-    measurementId: "G-P51ZMMNK95"
+  apiKey: "AIzaSyBNGYwp9XI5C2jbsYZjOJc3ydVN-2g3vjI",
+  authDomain: "readlater-708be.firebaseapp.com",
+  databaseURL: "https://readlater-708be.firebaseio.com",
+  projectId: "readlater-708be",
+  storageBucket: "readlater-708be.appspot.com",
+  messagingSenderId: "264923569873",
+  appId: "1:264923569873:web:dbf0fb4f38757115d52ff0",
+  measurementId: "G-Z7YC9XXFKE"
 };
 
-firebase.initializeApp(firebaseConfig);
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app(); // if already initialized, use that one
+}
+
 const db = firebase.firestore();
 export { db };
 export default firebase;
